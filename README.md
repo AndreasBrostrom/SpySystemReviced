b0890fc7dfc8bb4df7647d62a69c4df8de1cb8cf
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-01-08 17:17:10 +0100

    Deployment

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Spy System Reviced
+<a href="https://github.com/AndreasBrostrom/SpySystemReviced/releases/latest"><img src="https://img.shields.io/github/release/AndreasBrostrom/SpySystemReviced.svg?style=for-the-badge&label=Release%20Build" alt="Release Build Version"></a>
+<a href="https://github.com/AndreasBrostrom/SpySystemReviced/releases/"><img src="https://img.shields.io/github/release/AndreasBrostrom/SpySystemReviced/all.svg?style=for-the-badge&label=Pre-release" alt="Pre release and or current build version"></a>
+<a href="https://github.com/AndreasBrostrom/SpySystemReviced/tags"><img src="https://img.shields.io/github/tag/AndreasBrostrom/SpySystemReviced.svg?style=for-the-badge&colorB=df2d00&label=Latest%20Tag" alt="Dev-build or the latest tag of the current build."></a><br>
+<a href="https://github.com/AndreasBrostrom/SpySystemReviced/releases/latest"><img src="https://img.shields.io/github/downloads/AndreasBrostrom/SpySystemReviced/total.svg?style=for-the-badge&label=Downloads" alt="SpySystemReviced Downloads"></a>
+<a href="https://steamcommunity.com/sharedfiles/filedetails/?id=2747306152"><img src="https://img.shields.io/endpoint.svg?url=https%3A%2F%2Fshieldsio-steam-workshop.jross.me%2F2747306152&style=for-the-badge" alt="Steam Workshop"></a>
+<a href="https://github.com/AndreasBrostrom/SpySystemReviced/issues"><img src="https://img.shields.io/github/issues-raw/AndreasBrostrom/SpySystemReviced.svg?style=for-the-badge&label=Issues" alt="SpySystemReviced Issues"></a>
+
+This Hearts of Iron 4 mod revices the spy system and make it more hands off and some what more barable.
+The main feature is the removal for operation agent requirement and instead adds a cost of civilian factories to all operations.
---
+++ scripts/deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+VERSION=$*
+PROJECT_NAME="SpySystemReviced" 
+PROJECT_NAME_SPACED="Spy System Reviced" 
+PROJECT_FILES=("common" "events" "gfx" "interface" "localisation" "descriptor.mod" "LICENSE" "README.md", "thumbnail.png")
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_ROOT_DIR="$(dirname "$SCRIPT_DIR")"
+BUILD_FOLDER="$PROJECT_ROOT_DIR/release/build"
+
+cd $PROJECT_ROOT_DIR
+
+mkdir -p $BUILD_FOLDER/$PROJECT_NAME
+
+#python3 $SCRIPT_DIR/make.py
+
+echo "Making release"
+for I in ${PROJECT_FILES[@]}
+do
+  [ -d "$I" ] || [ -f "$I" ] && echo "  copying: $I for release"
+  [ -d "$I" ] || [ -f "$I" ] && cp -Rp "$I" "$BUILD_FOLDER/$PROJECT_NAME/."
+done
+
+echo "Writing descriptor.mod"
+[ ! -z $VERSION ] && sed -i "s/0.0.0/${VERSION}/" $BUILD_FOLDER/$PROJECT_NAME/descriptor.mod
+sed -i "s/ DevBuild//" $BUILD_FOLDER/$PROJECT_NAME/descriptor.mod
+sed -i "s/_DevBuild//" $BUILD_FOLDER/$PROJECT_NAME/descriptor.mod
+
+sed -i "s/UTNH_DEV/Ultimate Tech Tree : New Horizon/" $BUILD_FOLDER/$PROJECT_NAME/descriptor.mod
+
+cp $BUILD_FOLDER/$PROJECT_NAME/descriptor.mod $BUILD_FOLDER/$PROJECT_NAME.mod
+
+# Readme
+[ -f "$PROJECT_ROOT_DIR/release/$PROJECT_NAME/README.md" ] && echo "Writing README.md"
+[ -f "$PROJECT_ROOT_DIR/release/$PROJECT_NAME/README.md" ] && sed -i "s/# $PROJECT_NAME_SPACED/# $PROJECT_NAME_SPACED v${VERSION}/" $PROJECT_ROOT_DIR/release/$PROJECT_NAME/README.md
+[ -f "$PROJECT_ROOT_DIR/release/$PROJECT_NAME/README.md" ] && sed -i "2,8d" $PROJECT_ROOT_DIR/release/$PROJECT_NAME/README.md
+
+echo "Building archive"
+cd $BUILD_FOLDER
+zip $PROJECT_NAME.zip -r *
+
+echo "Cleaning up"
+mv $BUILD_FOLDER/$PROJECT_NAME $BUILD_FOLDER/../$PROJECT_NAME
+mv $BUILD_FOLDER/$PROJECT_NAME.zip $BUILD_FOLDER/../$PROJECT_NAME.zip
+
+rm -rf $BUILD_FOLDER
